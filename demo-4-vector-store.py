408f04f32c2b4d50fc7b7411977bf31dc9c826a9
CuberMessenger <cuber_messenger@hotmail.com>
2025-06-20 14:35:22 +0800

    vector store demo passed

--- demo-4-vector-store.py
+++ demo-4-vector-store.py
@@ -2,8 +2,6 @@ import os
 import tqdm
 import asyncio
 
-import tqdm.asyncio
-
 from utility import *
 from langchain_core.vectorstores import InMemoryVectorStore
 from langchain_community.document_loaders import PyPDFLoader
@@ -29,23 +27,29 @@ def main():
 
     pages = load_pdf(pdf_path)
 
-    print(pages[10])
+    # print(pages[10])
 
-    splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    splitter = CharacterTextSplitter(chunk_size=512, chunk_overlap=128)
     chunks = splitter.split_documents(pages)
 
     print(f"Total chunks: {len(chunks)}")
 
-    print(chunks[10])
+    # print(chunks[10])
 
-    vector_store.add_documents(chunks[9:12])
+    # Monkey-patch the vector store object to specify the embedding dimension
+    monkey_patch_embedding_size(vector_store)
 
-    print(f"Total vectors: {len(vector_store)}")
+    vector_store.add_documents(documents=chunks, output_dimensionality=3072)  # Need TUN mode to go through proxy
 
-    docs = vector_store.similarity_search("On April 13, 1867, what's the speed and location of Scotia?", k=2)
+    docs = vector_store.similarity_search(
+        "On April 13, 1867, what's the speed and location of Scotia?", k=2
+    )
     for doc in docs:
+        print("#" * 0x7F)
         print(doc)
         print()
+
+    vector_store.dump(r"TTLUTS-First-8-Chapters-vector-store.json")
 
 
 if __name__ == "__main__":
--- utility.py
+++ utility.py
@@ -38,7 +38,7 @@ class LocalEnvironment:
         if LocalEnvironment.GOOGLE_API_KEY is None:
             LocalEnvironment.load_environment()
         return LocalEnvironment.GOOGLE_API_KEY
-    
+
     @staticmethod
     def get_google_cse_id():
         if LocalEnvironment.GOOGLE_CSE_ID is None:
@@ -59,7 +59,8 @@ def get_google_model(name="gemini-2.0-flash"):
 
     return model
 
-def get_google_embedding_model(name="models/text-embedding-004"):
+
+def get_google_embedding_model(name="models/gemini-embedding-exp-03-07"):
     api_key = LocalEnvironment.get_google_api_key()
 
     embedding_model = GoogleGenerativeAIEmbeddings(
@@ -72,3 +73,54 @@ def get_google_embedding_model(name="models/text-embedding-004"):
 
     return embedding_model
 
+
+def monkey_patch_embedding_size(in_memory_vector_store):
+    import uuid
+    import types
+
+    from langchain_core.documents import Document
+    from typing import Any, Optional
+    from collections.abc import Iterator
+
+    def add_documents_patched(
+        self,
+        documents: list[Document],
+        ids: Optional[list[str]] = None,
+        output_dimensionality: Optional[int] = None,  # This line is added
+        **kwargs: Any,
+    ) -> list[str]:
+        """Add documents to the store."""
+        texts = [doc.page_content for doc in documents]
+        vectors = self.embedding.embed_documents(
+            texts, output_dimensionality=output_dimensionality
+        )  # This line is modified
+
+        if ids and len(ids) != len(texts):
+            msg = (
+                f"ids must be the same length as texts. "
+                f"Got {len(ids)} ids and {len(texts)} texts."
+            )
+            raise ValueError(msg)
+
+        id_iterator: Iterator[Optional[str]] = (
+            iter(ids) if ids else iter(doc.id for doc in documents)
+        )
+
+        ids_ = []
+
+        for doc, vector in zip(documents, vectors):
+            doc_id = next(id_iterator)
+            doc_id_ = doc_id or str(uuid.uuid4())
+            ids_.append(doc_id_)
+            self.store[doc_id_] = {
+                "id": doc_id_,
+                "vector": vector,
+                "text": doc.page_content,
+                "metadata": doc.metadata,
+            }
+
+        return ids_
+
+    in_memory_vector_store.add_documents = types.MethodType(
+        add_documents_patched, in_memory_vector_store
+    )
